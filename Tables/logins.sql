0bd2e154e96c90ad3542037ba47c7dfd55aa9ec7
hernms <hernms@miamioh.edu>
2024-03-09 19:19:31 -0500

    Adding the database

---
+++ Tables/logins.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE `logins` (
+  `ID` INT NOT NULL,
+  `Username` VARCHAR(255) NOT NULL,
+  `Password` VARCHAR(255) NOT NULL,
+  PRIMARY KEY (ID)
+)
+ENGINE = INNODB,
+CHARACTER SET utf8mb4,
+COLLATE utf8mb4_0900_ai_ci,
+COMMENT = 'Database to store login information';
+
+ALTER TABLE `logins` 
+  ADD UNIQUE INDEX Username_UNIQUE(Username);
